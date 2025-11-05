bb6e02bd99de573e38c7de75f33bbeb368d6088c
Atharv <atharvn393@gmail.com>
2025-11-05 19:45:36 +0530

    add insights API route and pipeline for its API endpoints

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from routes.companies import companies_bp
+from routes.insights import insights_bp
 from flask_cors import CORS
 from flask import Flask
 from dotenv import load_dotenv
@@ -10,6 +11,7 @@ load_dotenv()
 app = Flask(__name__)
 CORS(app)
 app.register_blueprint(companies_bp, url_prefix="/companies")
+app.register_blueprint(insights_bp, url_prefix="/insights")
 
 
 @app.route("/")
